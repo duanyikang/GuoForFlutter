e5e05ca01f7a7a9c304431725a0f7929bbfc37b1
duanyikang <574281045@qq.com>
2019-08-11 16:58:20 +0800

    添加动效

--- lib/widget/destinationSearchWidget.dart
+++ lib/widget/destinationSearchWidget.dart
@@ -36,10 +36,11 @@ class _DestinationSearchWidget extends State<DestinationSearchWidget> {
 
   @override
   Widget build(BuildContext context) {
+    print("我要的：${currentValue}");
     return Container(
         color: Colors.white,
         child: Container(
-          color: Color.fromARGB(255 - currentValue.toInt(), 255, 97, 0),
+          color: Color.fromARGB(255 - (currentValue*255).toInt(), 255, 97, 0),
           padding: EdgeInsets.fromLTRB(
               10, MediaQuery.of(context).padding.top + 10, 10, 0),
           child: Column(
@@ -48,20 +49,24 @@ class _DestinationSearchWidget extends State<DestinationSearchWidget> {
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Expanded(
+                  SizedBox(
+                    height: 30,
+                    width:  (1-currentValue)*50,
                     child: Text(
                       '北京',
                       style: TextStyle(
+                        backgroundColor: Colors.black,
                           color: Colors.white,
                           fontSize: 20,
                           fontWeight: FontWeight.bold),
                     ),
-                    flex: 1,
                   ),
-                  Expanded(
+                  SizedBox(
+                    height: 30,
+                    width: currentValue*50+280,
                     child: Container(
-                        height: 30,
                         decoration: new BoxDecoration(
+
                           border: Border.all(color: Colors.orange),
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(30.0),
@@ -76,7 +81,6 @@ class _DestinationSearchWidget extends State<DestinationSearchWidget> {
                                   icon: Icon(Icons.search),
                                   labelText: '搜索当地玩乐/美食/路线等'),
                             ))),
-                    flex: 4,
                   )
                 ],
               ),
@@ -98,7 +102,7 @@ class _DestinationSearchWidget extends State<DestinationSearchWidget> {
 
   void _handleValueChanged() {
     setState(() {
-      currentValue = widget.data.value;
+      currentValue = widget.data.value / MAX;
     });
   }
 }
